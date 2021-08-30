ad682baefd748443f9cc912e87c2943021f9c280
hithacker <hithacker@gmail.com>
2021-08-30 15:57:05 +0530

    Fixing auth issue

--- src/main/java/org/openlmis/stockmanagement/service/EswatiniStockEventPostProcessor.java
+++ src/main/java/org/openlmis/stockmanagement/service/EswatiniStockEventPostProcessor.java
@@ -7,6 +7,8 @@ import org.openlmis.stockmanagement.service.notifier.EswatiniStockAdjustmentNoti
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
 
 @Component(value = "EswatiniStockEventPostProcessor")
@@ -24,12 +26,19 @@ public class EswatiniStockEventPostProcessor implements StockEventPostProcessor 
   @Override
   public void process(StockEventDto stockEventDto) {
     XLOGGER.debug("EswatiniStockEventPostProcessor init");
-    if (stockEventDto.isPhysicalInventory()) {
-      XLOGGER.debug("Trying to notify all users about the physical inventory update");
-      eswatiniPhysicalInventoryNotifier.notify(stockEventDto);
+    OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder
+            .getContext()
+            .getAuthentication();
+    if (!authentication.isClientOnly()) {
+      if (stockEventDto.isPhysicalInventory()) {
+        XLOGGER.debug("Trying to notify all users about the physical inventory update");
+        eswatiniPhysicalInventoryNotifier.notify(stockEventDto);
+      } else {
+        XLOGGER.debug("This event is not a physical inventory");
+        eswatiniStockAdjustmentNotifier.notify(stockEventDto);
+      }
     } else {
-      XLOGGER.debug("This event is not a physical inventory");
-      eswatiniStockAdjustmentNotifier.notify(stockEventDto);
+      XLOGGER.debug("Skip notifying for client only api call");
     }
   }
 }
